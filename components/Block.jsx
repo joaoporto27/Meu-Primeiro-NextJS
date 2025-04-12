80a4f8a7bbf253b34d672377d5f3cb450e83ea79
joaoporto27 <joao.v.sales15@aluno.senai.br>
2025-04-12 12:09:22 -0300

    Feat: atividade refeita

---
+++ components/Block.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Block.module.css';
+
+export	default	function Block( { title, content  } )	{
+    return	(
+        <div className={styles.block}>
+            <h2>{title}</h2>
+            <p>{content}</p>
+        </div>
+    );
+}
